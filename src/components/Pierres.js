d5aa777fbfa6377fe67a609839a1eb72a19e81e5
Imad Lhannouni <lhannouni05@gmail.com>
2024-05-19 12:59:14 +0100

    modif

--- src/components/Pierres.js
+++ src/components/Pierres.js
@@ -3,6 +3,7 @@ import './Pierres.css';
 import minerals from '../images/mineral.PNG';
 import selenite from '../images/selenite.PNG';
 import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
 const Pierres = () => {
   return (
@@ -25,9 +26,36 @@ const Pierres = () => {
             </div>
           </div>
         </div>
+        <div className="col-md-6 mb-4">
+          <div style={{ width: '130%', height: '500px', position: 'relative' }}>
+            <ReactPlayer
+              url={require('../videos/background2.mp4')}
+              playing={true}
+              loop={true}
+              muted={true}
+              width="100%"
+              height="100%"
+              style={{ position: 'absolute', top: 0, left: 0 }}
+            />
+          </div>
+        </div>
+        <div className="col-md-6 mb-4">
+          <div style={{ width: '70%', height: '500px', position: 'relative' }}>
+            <ReactPlayer
+              url={require('../videos/background.mp4')}
+              playing={true}
+              loop={true}
+              muted={true}
+              width="100%"
+              height="100%"
+              style={{ position: 'absolute', top: 0, left: 0 }}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
 };
 
 export default Pierres;
+
